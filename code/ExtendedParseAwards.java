3e3756a6c5a045435bc59035c490bb35b5ead9e7
Linh Ngo <lngo@wcupa.edu>
2020-07-16 10:53:24 -0400

    Update ExtendedParseAwards.java

--- code/ExtendedParseAwards.java
+++ code/ExtendedParseAwards.java
@@ -3,6 +3,26 @@ import java.io.*;
 
 public class ExtendedParseAwards {
 
+  public static String[] stateListBR(String filename){
+    String states = "";
+    try {
+      BufferedReader br = new BufferedReader(new FileReader(filename));      
+      br.readLine();
+      String current = null;
+      while ((current = br.readLine()) != null) {
+        String[] award = current.split("\",\"");
+        if (!states.contains(award[7])){
+          states += award[7] + " ";
+        }
+      }
+      return states.split(" ");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+
+
   public static String[] stateList(String filename){
     String states = "";
     try {
@@ -23,7 +43,8 @@ public class ExtendedParseAwards {
   }
 
   public static void main(String[] args) {
-    String[] fundedStates = stateList("Awards_CISE_2019.csv");
+    //String[] fundedStates = stateList("Awards_CISE_2019.csv");
+    String[] fundedStates = stateListBR("Awards_CISE_2019.csv");
     for (int i = 0; i < fundedStates.length; i++) {
       System.out.println(fundedStates[i]);
     }
